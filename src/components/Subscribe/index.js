cdbf9d7539f3402824ef50e32c4b5fd39ff5aea5
Tilen Faganel <tilen.faganel@me.com>
2018-04-13 18:11:21 +0200

    Added honeypot to the netlify forms

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -3,54 +3,66 @@ import { Box } from 'grid-styled'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faRssSquare from '@fortawesome/fontawesome-free-solid/faRssSquare'
 
-import { Input, Button, InsideLink } from '../Primitives' 
+import { Input, Button, InsideLink } from '../Primitives'
 
 const SubscribeSection = Box.extend`
   text-align: center;
   border-top: 1px solid ${props => props.theme.colors.divider};
 `
 
-const encode = data => Object.keys(data)
-  .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-  .join('&');
+const encode = data =>
+  Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
 
 class Subscribe extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {};
+    this.state = {}
   }
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   handleSubmit = e => {
-
     fetch('/', {
       method: 'POST',
-      headers: { 'Content-Type': "application/x-www-form-urlencoded" },
-      body: encode({ 'form-name': 'subscribe', ...this.state })
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'subscribe', ...this.state }),
     })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
+      .then(() => alert('Success!'))
+      .catch(error => alert(error))
 
-    e.preventDefault();
-  };
+    e.preventDefault()
+  }
 
   render() {
-
     return (
       <SubscribeSection mt={4} pt={4}>
-        <p>
-          Don't miss out!
-        </p>
-        <form name="subscribe" method="post" data-netlify="true" onSubmit={this.handleSubmit}>
-          <Input type="email" name="email" required placeholder="you@email.com" width={250} onChange={this.handleChange} />
-          <Button type="submit" ml={[0, 3]} mt={[3, 0]}>Subscribe</Button>
+        <p>Don't miss out!</p>
+        <form
+          name="subscribe"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <input name="bot-field" style={{display: 'none'}} />
+          <Input
+            type="email"
+            name="email"
+            required
+            placeholder="you@email.com"
+            width={250}
+            onChange={this.handleChange}
+          />
+          <Button type="submit" ml={[0, 3]} mt={[3, 0]}>
+            Subscribe
+          </Button>
         </form>
-        <InsideLink to='/feed.xml'>
+        <InsideLink to="/feed.xml">
           <FontAwesomeIcon icon={faRssSquare} size="lg" />
         </InsideLink>
       </SubscribeSection>
